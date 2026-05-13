16b17298594939a226b8102ba0e322214125db89
Alex <Sasha2001-89@mail.ru>
2026-05-13 17:57:35 +0500

    Добавил автоматический запуск тестов через GitHub Actions

---
+++ test_calc.py
@@ -0,0 +1,14 @@
+# test_calc.py
+from calc import add, subtract
+
+def test_add():
+    # Проверяем, что 2 + 3 действительно равно 5
+    assert add(2, 3) == 5
+
+def test_subtract():
+    # Проверяем вычитание
+    assert subtract(10, 5) == 5
+
+def test_add_negative():
+    # Проверим отрицательные числа
+    assert add(-1, -1) == -2
